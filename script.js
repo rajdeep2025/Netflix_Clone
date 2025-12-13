dbfc75e3dfec53406df270e05eefc35b779739ef
rajdeep2025 <dequinox25@gmail.com>
2025-12-13 22:08:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,18 @@ passwordInput.addEventListener('input', function() {
     passwordError.classList.remove('show');
     loginError.classList.remove('show');
 });
+<script>
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach(item => {
+        const question = item.querySelector(".faq-question");
+
+        question.addEventListener("click", () => {
+            item.classList.toggle("active");
+
+            const icon = question.querySelector(".icon");
+            icon.textContent = item.classList.contains("active") ? "×" : "+";
+        });
+    });
+</script>
+
