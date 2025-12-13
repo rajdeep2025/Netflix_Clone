9f9830653af77bfaf7b24b8bf71fe3635b350f9f
rajdeep2025 <dequinox25@gmail.com>
2025-12-13 17:13:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function login() {
+    // Get values from input fields
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Simple validation (fake auth)
+    if (username === "admin" && password === "netflix") {
+
+        // Save login state in browser
+        localStorage.setItem("loggedIn", "true");
+
+        // Redirect to home page
+        window.location.href = "home.html";
+
+    } else {
+        // Show error message
+        document.getElementById("error").innerText =
+            "Invalid username or password";
+    }
+}
